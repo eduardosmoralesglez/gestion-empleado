ffe6aec19048b7b05b2c79c91d6a82c1a325a8b5
eduardosmoralesglez <eduardomoralez24@gmail.com>
2025-01-30 18:38:15 +0000

    Avance de la tarea

--- proyecto/src/main/java/es/ies/puerto/herencia/Programa.java
+++ proyecto/src/main/java/es/ies/puerto/herencia/Programa.java
@@ -1,17 +1,13 @@
 package es.ies.puerto.herencia;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import es.ies.puerto.herencia.model.Desarrollador;
+import es.ies.puerto.herencia.model.Empleado;
+
 
 public class Programa {
-    static Persona persona;
-    static Crud repositorio;
+    static Empleado desarrollador = new Desarrollador("Carlos", 30, 3000);
     
     public static void main(String[] args) {
-        repositorio = new Bbdd();
-        persona = new Administrador("Pepe", "00000000H",
-         "aa@ee.com", "ss", Arrays.asList("eliminar"));
-        System.out.println("Valor de persona:"+persona);
-        System.out.println("Almacena:"+repositorio.add(persona));
+        System.out.println(desarrollador);
     }
 }
