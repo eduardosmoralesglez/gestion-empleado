7898c1a5e02d67da8c26f5f9654ea6046e7a228d
EduardoSMoralesGlez <eduardomoralez24@gmail.com>
2025-02-03 23:16:52 +0000

    c

---
+++ proyecto/src/main/java/es/ies/puerto/EjemploFile.java
@@ -0,0 +1,16 @@
+package es.ies.puerto;
+
+import es.ies.puerto.model.Persona;
+
+public class EjemploFile {
+    
+    public static void main(String[] args) {
+        
+    
+    
+    
+    
+    
+    
+    }    
+}
